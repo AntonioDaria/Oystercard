c3ebf37b29fcf0dd58127e2c73229c5ccf081227
Antonio <antoniodaria@msn.com>
2018-09-10 15:37:53 +0100

    Added balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,16 @@
-describe Oystercard do 
+require 'oystercard'
+
+
+# I want money on my card
+
+describe Oystercard do
+subject(:oystercard) { described_class.new }
+
+  it "checks balance" do
+    expect(oystercard).to respond_to(:balance)
+  end
+  it "checks that it has 0 balance" do
+    expect(oystercard.balance).to eq 0
+  end
+
 end
