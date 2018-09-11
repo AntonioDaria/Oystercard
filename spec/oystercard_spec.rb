746e1e49d89604f97043ccce2de28a9bdfa7e56c
Cameron Whitehead <camjw119@gmail.com>
2018-09-11 13:12:24 +0100

    refactored oystercard spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,24 @@
 require 'oystercard'
 
-
-# I want money on my card
-
 describe Oystercard do
 subject(:oystercard) { described_class.new }
 
-  it "checks balance" do
-    expect(oystercard).to respond_to(:balance)
+  context "card has zero balance" do
+    it "checks that it has 0 balance" do
+      expect(oystercard.balance).to eq 0
+    end
   end
 
-  it "checks that it has 0 balance" do
-    expect(oystercard.balance).to eq 0
+  context "card has positive balance" do
+    describe '#pay_fare' do
+      it "deducts money" do
+        oystercard.top_up(10)
+        expect { oystercard.pay_fare 5 }.to change{ oystercard.balance}.by -5
+      end
+    end
   end
 
   describe "top_up" do
-    it "responds to top_up" do
-      expect(oystercard).to respond_to(:top_up).with(1).argument
-    end
-
     it "adds money to the oystercard" do
       expect{ oystercard.top_up 10 }.to change{ oystercard.balance }.by 10
     end
@@ -27,17 +27,6 @@ subject(:oystercard) { described_class.new }
       limit = Oystercard::LIMIT
       oystercard.top_up(limit)
       expect { oystercard.top_up 1 }.to raise_error "Cannot exceed £90!"
-    end
-
-    describe '#pay_fare' do
-      it 'responds to pay_fare' do
-        expect(oystercard).to respond_to(:pay_fare).with(1).argument
-      end
-
-      it "deducts money" do
-        oystercard.top_up(10)
-        expect { oystercard.pay_fare 5 }.to change{ oystercard.balance}.by -5 
-      end
     end
   end
 
